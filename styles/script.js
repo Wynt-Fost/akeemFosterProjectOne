d80cf2df631a43cf871f6fd16a13f513fe4e9d5c
Akeem <akeemfoster2@gmail.com>
2021-12-15 23:03:55 -0500

    adding comments :o

--- styles/script.js
+++ styles/script.js
@@ -1,7 +1,17 @@
+// 1. create variables to store stuff
+// 2. Be able to click the hamburger icon so that it registers as  a click event
+// 3. once click tell code to make my hidden nav bar visible.
+
+
+
+// creating a const for my hamburger icon
 const hamburgerIcon = document.querySelector(".burger");
+
+// creating a const fot my nav
 const navMenu = document.querySelector(".mainNav");
 
 
+// making my icon clickable while adding the ability to show my hidden nav bar once clicked.
 hamburgerIcon.addEventListener('click', function () {
     document.getElementById('mainNav').style.visibility = 'visible';
 });
