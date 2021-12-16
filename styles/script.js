38a4b3b5f086d7d26000183f4bdcc07bd4a67c32
Akeem <akeemfoster2@gmail.com>
2021-12-15 22:08:31 -0500

    adding finishing touches

--- styles/script.js
+++ styles/script.js
@@ -5,3 +5,5 @@ const navMenu = document.querySelector(".mainNav");
 hamburgerIcon.addEventListener('click', function () {
     document.getElementById('mainNav').style.visibility = 'visible';
 });
+
+
